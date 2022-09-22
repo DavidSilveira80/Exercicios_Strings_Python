8e888a193267279a4bcce6683d14f7c3a6ae8988
DavidSilveira80 <davidpscontato@gmail.com>
2022-09-22 18:51:18 -0300

    Implementada função que compara Strings.

---
+++ compara_strings.py
@@ -0,0 +1,11 @@
+def compara_strings(string1: str, string2: str) -> bool:
+    if string1 == string2:
+        resp = True
+    else:
+        resp = False
+    return resp
+
+
+if __name__ == '__main__':
+    assert compara_strings('casa', 'prédio') == False
+    assert compara_strings('David', 'David') == True
